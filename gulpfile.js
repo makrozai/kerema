68c60c6c950838b6b6541a58aadedb8bdf6bd7cb
Marco <marco.condoric@gmail.com>
2018-07-19 14:51:03 -0500

    agregue editor config y modifique el gulpfile para que escuche todos los cambios de archivos pug scss y js en carpetas

--- gulpfile.js
+++ gulpfile.js
@@ -54,8 +54,8 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('source/js/*.js', ['uglify']);
-    gulp.watch('source/scss/*.scss', ['scss']);
-    gulp.watch('source/templates/*.pug', ['pug']);
+    gulp.watch('source/js/**/*.js', ['uglify']);
+    gulp.watch('source/scss/**/*.scss', ['scss']);
+    gulp.watch('source/templates/**/*.pug', ['pug']);
     gulp.watch('source/img/*.{jpg,jpeg,png,gif}', ['imagemin']);
 });
