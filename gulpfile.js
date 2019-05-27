2872ded0749b6e33cb55d7983f05f4f4cd8d9c0b
programador 2 <programador2@iMac-de-La-2.local>
2019-05-27 09:27:33 -0500

    cambia funcion fontmin y agrega concat en gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     pug = require('gulp-pug'),
     plumber = require('gulp-plumber'),
     imagemin = require('gulp-imagemin'),
+    concat = require('gulp-concat'),
     webserver = require('gulp-webserver'),
     autoprefixer = require('gulp-autoprefixer'),
     fontmin = require('gulp-fontmin');
@@ -21,6 +22,7 @@ gulp.task('uglify', function() {
           presets: ['es2015']
         }))
         .pipe(uglify())
+        .pipe(concat('main.js'))
         .pipe(gulp.dest('dist/js'));
 });
 
@@ -43,7 +45,7 @@ gulp.task('pug', function() {
 
 // comprime archivos de imagen(JPG,JPEG, PNG,GIF,SVG)
 gulp.task('imagemin', function() {
-    gulp.src('source/img/*.{jpg,jpeg,png,gif,svg}')
+    gulp.src('source/img/**/*.{jpg,jpeg,png,gif,svg}')
     .pipe(plumber())
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
@@ -51,8 +53,8 @@ gulp.task('imagemin', function() {
 
 // genera archivos de fuente compatibles a partir de TTF
 gulp.task('fontmin', function() {
-    gulp.src('source/fonts/**/*.ttf')
-    .pipe(fontmin({text: '天地玄黄 宇宙洪荒'}))
+    gulp.src('source/fonts/**/*.{ttf,eot,svg,woff}')
+    //.pipe(fontmin({text: '天地玄黄 宇宙洪荒'}))
     .pipe(gulp.dest('dist/fonts'));
 });
 
